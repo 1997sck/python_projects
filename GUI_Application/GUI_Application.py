04a1fb5bbc5ce5e546f7d4f4aa6a5ba8ec236319
1997sck <79891854+1997sck@users.noreply.github.com>
2021-04-28 19:22:35 +0530

    Add files via upload

---
+++ GUI_Application/GUI_Application.py
@@ -0,0 +1,14 @@
+
+from tkinter import *
+
+# create root window
+root = Tk()
+
+# root window title and dimension
+root.title("Welcome to python")
+# Set geometry (widthxheight)
+root.geometry('350x200')
+
+# all widgets will be here
+# Execute Tkinter
+root.mainloop()
