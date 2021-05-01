24e3605833340d0d31e9fac851565b4d4237a271
1997sck <79891854+1997sck@users.noreply.github.com>
2021-05-01 15:01:17 +0530

    Add files via upload

---
+++ Speeed_tester/Speed_Tester.py
@@ -0,0 +1,25 @@
+# Import Module
+import urllib.request, urllib.error, urllib.parse
+import time
+import sys
+
+
+def main(multi_times):
+    starting_check_point = time.time()  # Timer Starting Point
+    print("[+] Test Started On : ", time.ctime(starting_check_point))
+    download_data = []  # For Collecting Downloaded Data
+    for i in range(multi_times):  # Use Any Website Page
+        data = urllib.request.urlopen("https://en.wikipedia.org/wiki/Internet").read()
+        download_data.append(sys.getsizeof(data))  # Calculating WebPage Size
+    ending_check_point = time.time()  # Time Closing Point
+    print("[+] Test Closed On : ", time.ctime(ending_check_point))
+    total_data_size = sum(download_data)  # Calculating Total Size of Downloaded data
+    total_time_used = ending_check_point - starting_check_point  # Calculating Total Time
+    one_byte_equal_to = 1024
+    speed = int(total_data_size / total_time_used / one_byte_equal_to)  # Calculating Average Speed
+    print("[+] Average InterNet Speed : {} KB".format(speed))
+
+
+if __name__ == '__main__':
+    main(5)
+input('[ Thanks For USing! Have A Nice Day ]')
